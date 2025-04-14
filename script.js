0047e8bebaa9410da501b14b356b44ab9455dc2f
Himanshu Singh <db3131444@gmail.com>
2025-04-15 03:38:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,108 +1,126 @@
-let btnRef = document.querySelectorAll(".button-option");
-let popupRef = document.querySelector(".popup");
-let newgameBtn = document.getElementById("new-game");
-let restartBtn = document.getElementById("restart");
-let msgRef = document.getElementById("message");
-//Winning Pattern Array
-let winningPattern = [
-  [0, 1, 2],
-  [0, 3, 6],
-  [2, 5, 8],
-  [6, 7, 8],
-  [3, 4, 5],
-  [1, 4, 7],
-  [0, 4, 8],
-  [2, 4, 6],
+const btnRef = document.querySelectorAll(".button-option");
+const popupRef = document.querySelector(".popup");
+const newgameBtn = document.getElementById("new-game");
+const restartBtn = document.getElementById("restart");
+const msgRef = document.getElementById("message");
+const xScore = document.getElementById("x-score");
+const oScore = document.getElementById("o-score");
+const difficultySelect = document.getElementById("difficulty");
+
+const clickSound = document.getElementById("click-sound");
+const winSound = document.getElementById("win-sound");
+const drawSound = document.getElementById("draw-sound");
+
+const winningPattern = [
+  [0, 1, 2], [0, 3, 6], [2, 5, 8],
+  [6, 7, 8], [3, 4, 5], [1, 4, 7],
+  [0, 4, 8], [2, 4, 6]
 ];
-//Player 'X' plays first
+
 let xTurn = true;
 let count = 0;
+let xWinCount = 0;
+let oWinCount = 0;
 
-//Disable All Buttons
 const disableButtons = () => {
   btnRef.forEach((element) => (element.disabled = true));
-  //enable popup
   popupRef.classList.remove("hide");
 };
 
-//Enable all buttons (For New Game and Restart)
 const enableButtons = () => {
   btnRef.forEach((element) => {
     element.innerText = "";
     element.disabled = false;
+    element.classList.remove("win");
   });
-  //disable popup
   popupRef.classList.add("hide");
 };
 
-//This function is executed when a player wins
-const winFunction = (letter) => {
+const winFunction = (letter, indices) => {
   disableButtons();
-  if (letter == "X") {
-    msgRef.innerHTML = "&#x1F389; <br> 'X' Wins";
+  indices.forEach(i => btnRef[i].classList.add("win"));
+  winSound.play();
+  if (letter === "X") {
+    msgRef.innerHTML = "🎉 <br> 'X' Wins";
+    xScore.innerText = ++xWinCount;
   } else {
-    msgRef.innerHTML = "&#x1F389; <br> 'O' Wins";
+    msgRef.innerHTML = "🎉 <br> 'O' Wins";
+    oScore.innerText = ++oWinCount;
   }
 };
 
-//Function for draw
 const drawFunction = () => {
   disableButtons();
-  msgRef.innerHTML = "&#x1F60E; <br> It's a Draw";
+  msgRef.innerHTML = "😎 <br> It's a Draw";
+  drawSound.play();
 };
 
-//New Game
+const winChecker = () => {
+  for (let i of winningPattern) {
+    let [a, b, c] = i;
+    let val1 = btnRef[a].innerText;
+    let val2 = btnRef[b].innerText;
+    let val3 = btnRef[c].innerText;
+    if (val1 !== "" && val1 === val2 && val2 === val3) {
+      winFunction(val1, [a, b, c]);
+      return true;
+    }
+  }
+  return false;
+};
+
+const aiMove = () => {
+  const difficulty = difficultySelect.value;
+  const empty = Array.from(btnRef).filter(btn => btn.innerText === "");
+
+  if (difficulty === "easy") {
+    const randBtn = empty[Math.floor(Math.random() * empty.length)];
+    randBtn.innerText = "O";
+    randBtn.disabled = true;
+    count++;
+  } else if (difficulty === "medium") {
+    for (let i = 0; i < btnRef.length; i++) {
+      if (btnRef[i].innerText === "") {
+        btnRef[i].innerText = "O";
+        btnRef[i].disabled = true;
+        count++;
+        break;
+      }
+    }
+  } else if (difficulty === "hard") {
+    const randBtn = empty[Math.floor(Math.random() * empty.length)];
+    randBtn.innerText = "O";
+    randBtn.disabled = true;
+    count++;
+  }
+
+  winChecker() || (count === 9 && drawFunction());
+};
+
+btnRef.forEach((element) => {
+  element.addEventListener("click", () => {
+    if (element.innerText !== "") return;
+    clickSound.play();
+    element.innerText = "X";
+    element.disabled = true;
+    count++;
+    if (!winChecker() && count < 9) {
+      setTimeout(aiMove, 500);
+    } else if (count === 9) {
+      drawFunction();
+    }
+  });
+});
+
 newgameBtn.addEventListener("click", () => {
   count = 0;
   enableButtons();
 });
+
 restartBtn.addEventListener("click", () => {
   count = 0;
   enableButtons();
+  xScore.innerText = oScore.innerText = xWinCount = oWinCount = 0;
 });
 
-//Win Logic
-const winChecker = () => {
-  //Loop through all win patterns
-  for (let i of winningPattern) {
-    let [element1, element2, element3] = [
-      btnRef[i[0]].innerText,
-      btnRef[i[1]].innerText,
-      btnRef[i[2]].innerText,
-    ];
-    //Check if elements are filled
-    //If 3 empty elements are same and would give win as would
-    if (element1 != "" && (element2 != "") & (element3 != "")) {
-      if (element1 == element2 && element2 == element3) {
-        //If all 3 buttons have same values then pass the value to winFunction
-        winFunction(element1);
-      }
-    }
-  }
-};
-
-//Display X/O on click
-btnRef.forEach((element) => {
-  element.addEventListener("click", () => {
-    if (xTurn) {
-      xTurn = false;
-      //Display X
-      element.innerText = "X";
-      element.disabled = true;
-    } else {
-      xTurn = true;
-      //Display Y
-      element.innerText = "O";
-      element.disabled = true;
-    }
-    //Increment count on each click
-    count += 1;
-    if (count == 9) {
-      drawFunction();
-    }
-    //Check for win on every click
-    winChecker();
-  });
-});
-//Enable Buttons and disable popup on page load
 window.onload = enableButtons;
